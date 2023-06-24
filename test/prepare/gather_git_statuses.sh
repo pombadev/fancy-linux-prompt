adfcc755a6576524726ef00583042ed4f8017d23
Steve <sweavo@gmail.com>
2023-06-24 07:35:39 +0100

    make the output prettier, and incidentally scramble all the uids

--- test/prepare/gather_git_statuses.sh
+++ test/prepare/gather_git_statuses.sh
@@ -1,5 +1,6 @@
 #/bin/env bash
 
+echo "info: expect a fatal because one folder is not a valid repo (from $0)"
 for folder in $(ls -d t-*)
 do 
     (
@@ -7,4 +8,5 @@ do
         git status --porcelain=2 --branch
     ) > out/$folder.status.txt
 done
+echo "info: done (from $0)"
 
