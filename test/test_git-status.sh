61c366781ad6f764e6f985ef278bcda106dcb6cc
Steve <sweavo@gmail.com>
2023-06-26 21:58:28 +0100

    factor out the system under test

--- test/test_git-status.sh
+++ test/test_git-status.sh
@@ -6,16 +6,7 @@ set -o pipefail
 
 btf_name=""
 # This function is the system under test (SUT)
-function git_status_branch {
-    readarray lines
-    for line in "${lines[@]}"
-    do
-        if grep -q "^# branch.head" <<< $line 
-        then
-            git_status_branch=$(sed 's/.*\.head //'<<<$line)
-        fi
-    done 
-}
+source ../git-status.fns
 # end SUT
 
 function get_flags_for_config {
@@ -26,12 +17,13 @@ function get_flags_for_config {
 
 source bash_test_framework.sh
 
-test_start "Branch name/clean repo"
+# Branch name tests
+test_start "clean repo"
 get_flags_for_config clean
 test "$git_status_branch" = "master" && test_pass "master branch Ok"
 test_end
 
-test_start "Branch name/switched repo"
+test_start "switched repo"
 get_flags_for_config switch-branch
 test "$git_status_branch" = "feature-branch" && test_pass "feature-branch branch Ok"
 test_end
@@ -41,4 +33,21 @@ get_flags_for_config headless
 test "$git_status_branch" = "(detached)" && test_pass "detached branch Ok"
 test_end
 
-test_summary
+# Dirty Worktree
+test_start "not added"
+get_flags_for_config clean
+$git_status_added_disk || test_pass "clean repo does not show added file"
+test_end
+
+test_start "added disk"
+get_flags_for_config added
+$git_status_added_disk && test_pass "detect added file on disk"
+test_end
+
+test_start "added multiple on disk"
+get_flags_for_config added2
+$git_status_added_disk && test_pass "detect added files on disk"
+test_end
+
+
+test_summary 
