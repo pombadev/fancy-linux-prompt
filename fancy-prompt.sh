cc9321de8506a53fc4ac6a186df3feec5bb09afd
Steve <sweavo@gmail.com>
2023-07-31 21:47:12 +0100

    integrate __Recurse into prompt setting script

--- fancy-prompt.sh
+++ fancy-prompt.sh
@@ -20,9 +20,73 @@ __set_prompt_command() {
             fi
         fi
         echo -n "("
+        if [[ "$git_status_ahead" -ne "0" ]]
+        then 
+            echo -n "${git_status_ahead}<"
+        fi
         echo -n "${git_status_branch}"
-        echo ")"
+        if [[ "$git_status_behind" -ne "0" ]]
+        then 
+            echo -n ">${git_status_behind}"
+        fi
+        echo -n ")"
 
+    }
+
+    function __recurse
+    {
+        local dir
+        local b
+        local d
+        local sep
+        sep=/
+
+        if [[ $# -eq 0 ]]
+        then
+            # base case: current dir
+            dir="$PWD"
+        else
+            # recursive case: given the path
+            dir="$1"
+        fi
+
+        if [[ "X$dir" == "X$HOME" ]]
+        then
+            # while recursing, reimplement the ~ convention
+            d=
+            b=\~
+            sep=
+        else
+            # work out my name and the path I want to recurse on
+            d=$(dirname $dir)
+            b=$(basename $dir)
+        fi    
+
+        if [[ "X$d" != "X/" && "X$d" != "X" ]]
+        then
+            # Depth-first recursion
+            __recurse $d
+        fi    
+
+        # Now draw myself to stdout
+        echo -n ${CS_PLAIN}${sep}
+        if [ -e "$dir/.git" ]
+        then  
+            __git_info
+        fi
+        
+        if [ -w "$dir" ]
+        then
+            echo -n "$CS_DIR_RW"
+        else    
+            echo -n "$CS_DIR_RO"
+        fi
+        echo -n $b
+        # At the top level, insert a newline
+        if [[ $# -eq 0 ]]
+        then
+            echo
+        fi
     }
 
     set_prompt_string() {
@@ -36,16 +100,12 @@ __set_prompt_command() {
             local FG_EXIT="$FG_RED"
         fi
 
-        PS1="$CS_DIR_RW"
-        PS1+="\\w"
-        PS1+="$RESET"
-        PS1+="$(__git_info)"
-        PS1+="$RESET"
-        PS1+=" ${COL_FG_BASE0}\\$ "
+        PS1=$(__recurse)" ${COL_FG_BASE0}\\$ "
     }
 
     PROMPT_COMMAND=set_prompt_string
 }
+
 # Skip if not interactive shell
 [[ $- == *i* ]] || return
 __set_prompt_command
